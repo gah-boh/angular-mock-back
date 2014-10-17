a9810935bad0b7cbc876cda11ed1c14d1f36ecd1
Gabriel Obregon <gabo.obregon@gmail.com>
2014-10-17 19:36:19 -0400

    Initial Commit.

---
+++ angular-mock-back.js
@@ -0,0 +1,41 @@
+;(function(window) {
+
+var baseConfig = {method: 'GET', code: 200};
+
+function angularMockBack(config) {
+	var urlMappings = config.mappings;
+
+	angular.module(config.moduleName)
+		.config(function($provide) {
+			$provide.decorator('$httpBackend', angular.mock.e2e.$httpBackendDecorator);
+		})
+		.run(['$httpBackend', '$window', function($httpBackend, $window) {
+			$httpBackend.whenGET(/\.html$/).passThrough();
+
+			var overrides = $window.location.search.slice(1).split('&');
+
+			urlMappings.forEach(function(mapping) {
+				var baseConfig = { method: 'GET', code: 200 };
+				var fullMapping = angular.extend({}, baseConfig, mapping);
+
+				if(overrides.length && mapping.hasOwnProperty('overrides')) {
+					overrides.filter(function(overrideName) {
+							return mapping.overrides.hasOwnProperty(overrideName);
+						})
+						.forEach(function(overrideName) {
+							angular.extend(fullMapping, mapping.overrides[overrideName]);
+						});
+				}
+
+				$httpBackend.when(fullMapping.method, fullMapping.url, fullMapping.data, fullMapping.headers)
+							.respond(fullMapping.code, fullMapping.body);
+			});
+
+		}]);
+
+}
+
+window.angularMockBack = angularMockBack;
+
+}(window));
+
